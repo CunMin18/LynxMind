801fc06b4c130e7147ae3a06ae3dc918ba4611dc
马艳绿 马 <2040266560@qq.com>
2025-11-17 14:34:50 +0800

    完善了制作功能

---
+++ src/client/java/org/ricey_yam/lynxmind/client/utils/game_ext/interaction/ComplexContainerType.java
@@ -0,0 +1,12 @@
+package org.ricey_yam.lynxmind.client.utils.game_ext.interaction;
+
+public enum ComplexContainerType{
+    NONE,
+    PLAYER_INFO,
+    CRAFTING_TABLE,
+    CHEST_BIG,
+    CHEST,
+    SMITHING_TABLE,
+    BREWING_STAND,
+    FURNACE
+}
